e1c61f5e8fcfc464a2be5ff8495af0064f4381b0
aujjwal300 <ujjwalagrawal300@gmail.com>
2022-01-24 22:19:06 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/Navbar";
+// import Footer from "./components/Footer";
+import ProductList from "./components/ProductList";
+import React,{useState} from "react";
 
 function App() {
+  const productList = [
+    {
+      price: 99999,
+      name: "IPhone 10S Max",
+      quantity: 0,
+    },
+    {
+      price: 9999,
+      name: "Redmi Note 10S Max",
+      quantity: 0,
+    }
+  ]
+
+  let [List, setProductList] = useState(productList)
+
+  const incrementQuanity = (index) =>{
+    let newProductList = [...List]
+    newProductList[index].quantity++;
+    setProductList(newProductList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <main className="container mt-5">
+        <ProductList productList={productList} incrementQuanity={incrementQuanity}/>
+      </main>
+      {/* <Footer/> */}
+    </>
   );
 }
 
