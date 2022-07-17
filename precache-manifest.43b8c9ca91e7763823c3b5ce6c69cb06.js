67a834fc1d3df34db0a1cec3dbd905aaba061d7f
aujjwal300 <ujjwalagrawal300@gmail.com>
2022-07-17 13:16:02 +0530

    Updates

---
+++ precache-manifest.43b8c9ca91e7763823c3b5ce6c69cb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ee97c265e7d721dee5c",
+    "url": "/addCart-firstReact-project/static/css/main.be25c1b2.chunk.css"
+  },
+  {
+    "revision": "3ee97c265e7d721dee5c",
+    "url": "/addCart-firstReact-project/static/js/main.3ee97c26.chunk.js"
+  },
+  {
+    "revision": "8cc5e16a4b0457892dee",
+    "url": "/addCart-firstReact-project/static/js/1.8cc5e16a.chunk.js"
+  },
+  {
+    "revision": "63570d3a107cc56cd9e1",
+    "url": "/addCart-firstReact-project/static/js/2.63570d3a.chunk.js"
+  },
+  {
+    "revision": "ceb5f292719051aa8c9d",
+    "url": "/addCart-firstReact-project/static/js/runtime~main.ceb5f292.js"
+  },
+  {
+    "revision": "147b0dc1063872044eb64bdb70f431f3",
+    "url": "/addCart-firstReact-project/index.html"
+  }
+];
